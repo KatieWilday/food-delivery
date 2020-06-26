28f60755635cdda3107ba16ede32fa027422d3f2
Katie Wilday <wildayk@gmail.com>
2020-06-26 13:58:27 -0400

    adding jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
 
   //Sticky Navigation
-  $()
-})
+  $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
+    },{
+      offset: '60px'
+  });
+
+  /* var waypoints = $('#handler-first').waypoint(function(direction) {
+    notify(this.element.id + ' hit 25% from top of window')
+  }, {
+    offset: '25%'
+  }) */
+});
